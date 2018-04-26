3d5eab1c88b6da7799234c93904b80e7b9b66009
li-yu-qiang <1243412864@qq.com>
2018-04-26 22:40:59 +0800

    Initial commit

---
+++ 第六次作业.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
+import matplotlib as mpl
+mpl.rcParams['font.sans-serif'] = ['KaiTi']
+plt.rcParams['axes.unicode_minus']=False
+
+plt.figure(figsize=(9,9))
+
+x = np.linspace(-10,10,2000)
+X = np.array([0,1,2,3,-1,-2,-3])
+Y = np.array([-1.22,1.85,3.22,10.29,2.21,3.72,8.7])
+
+
+def func(p, x):
+    a, b, c = p
+    return a * x ** 2 + b * x + c
+
+
+def err(p, x, y):
+    return func(p, x) - y
+
+p0=[1,1,1]
+exceptp=leastsq(err,p0,args=(X,Y))
+a,b,c=exceptp[0]
+
+y = a * x * x + b * x + c
+
+plt.scatter(X,Y, s=100, alpha=1.0, marker='o',label='数据点')
+plt.plot(x, y, color='r',linewidth=5, linestyle=":",markersize=20, label='拟合曲线')
+
+plt.legend(loc=0, numpoints=1)
+leg = plt.gca().get_legend()
+ltext  = leg.get_texts()
+plt.setp(ltext, fontsize='xx-large')
+
+plt.xlabel('安培/A')
+plt.ylabel('伏特/V')
+
+plt.xlim(x.min()* 1.1, x.max() * 1.1)
+plt.ylim(0, y.max() * 1.1)
+
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+#刻度字体大小
+plt.legend(loc='upper left')
+
+plt.show()
